d947898442559458645fe0890a564388a84117e4
Grzegorz Dyrda <grzegorz.dyrda@gmail.com>
2017-11-27 20:35:00 +0100

    Remove listeners as functions + code cleanup (closes #3)

--- src/main/kotlin/com/grzegorzdyrda/redux/Store.kt
+++ src/main/kotlin/com/grzegorzdyrda/redux/Store.kt
@@ -12,23 +12,13 @@ import kotlin.coroutines.experimental.CoroutineContext
  * @param initialState initial state of the Store
  * @param reducer a function that returns the next State, given the current State and the Action
  */
-class Store<out STATE, ACTION>(initialState: STATE,
-                               private val reducer: (STATE, ACTION) -> STATE) {
+class Store<STATE, ACTION>(initialState: STATE,
+                           private val reducer: (STATE, ACTION) -> STATE) {
 
-    //v1 - listeners as functions
-    private val listeners = mutableListOf<(STATE) -> Unit>()
-    //v2 - listeners as StoreSubscriber instances
     private val subscribers = mutableListOf<StoreSubscriber<STATE>>()
 
     private var currentState = initialState
     private var isDispatching = false
-
-    init {
-        // Notify listeners about the Initial State
-        // * No need for it since we immediately notify newly subscribed listeners in subscribe()
-        // * Also, at this point (in the constructor) there are certainly no listeners subscribed yet ;)
-        //listeners.forEach { it(currentState) }
-    }
 
     /**
      * Dispatches an Action. It's the only way to trigger a State change.
@@ -36,9 +26,8 @@ class Store<out STATE, ACTION>(initialState: STATE,
      * This method is thread-safe. Can be called from any thread.
      */
     fun dispatch(action: ACTION): ACTION {
-        if (isDispatching) {
+        if (isDispatching)
             throw IllegalStateException("Reducers may not dispatch actions! They should be pure functions - no side effects at all.")
-        }
 
         synchronized(this) {
             val newState = try {
@@ -50,21 +39,11 @@ class Store<out STATE, ACTION>(initialState: STATE,
             // Notify listeners only when State actually changed
             if (newState != currentState) {
                 currentState = newState
-                listeners.forEach { it(currentState) }
                 subscribers.forEach { it.onNewState(newState) }
             }
         }
 
         return action
-    }
-
-    /**
-     * Dispatches a *Command*. Commands are a way of notifying that some side-effect should be performed.
-     */
-    fun dispatchCommand(command: Any): Any {
-        subscribers.forEach { it.onCommandReceived(command) }
-
-        return command
     }
 
     /**
@@ -93,39 +72,27 @@ class Store<out STATE, ACTION>(initialState: STATE,
     }
 
     /**
-     * Returns the current State kept by the Store.
+     * Sends a Command. It's a way of notifying Subscribers that a *side-effect* should be performed.
+     *
+     * *Commands* do NOT perform any side-effects by themselves (thus can be safely called from
+     * inside Reducers). They only tell the Store that it should notify its Subscribers about the
+     * *intention* to perform certain side-effect.
      */
-    fun getState(): STATE {
-        if (isDispatching) {
-            throw IllegalStateException("You may not call store.getState() while the reducer is executing! The reducer has already received the State as an argument. Pass it down from the top Reducer instead of reading it from the store.")
-        }
+    fun sendCommand(command: Any): Any {
+        //TODO: Commands should be dispatched AFTER the reducer and
+        subscribers.forEach { it.onCommandReceived(command) }
 
-        return currentState
+        return command
     }
 
     /**
-     * Subscribes the [listener] to the State changes.
-     *
-     * Also, immediately fires the [listener], giving him the current State.
-     *
-     * @return Function to remove this listener
+     * Returns the current State kept by the Store.
      */
-    fun subscribe(listener: (STATE) -> Unit): () -> Unit {
-        if (isDispatching) {
-            throw IllegalStateException("You may not call store.subscribe() while the reducer is executing! If you would like to be notified after the store has been updated, subscribe from a component and invoke store.getState() in the callback to access the latest state.")
-        }
+    fun getState(): STATE {
+        if (isDispatching)
+            throw IllegalStateException("You may not call store.getState() while the Reducer is executing! The reducer has already received the State as an argument. Pass it down from the top Reducer instead of reading it from the store.")
 
-        listeners += listener
-        // Immediately notify newly subscribed listener about the current State.
-        listener(currentState)
-
-        // Return a function that allows users to remove this listener
-        return {
-            if (isDispatching) {
-                throw IllegalStateException("You may not unsubscribe from a store listener while the reducer is executing!")
-            }
-            listeners -= listener
-        }
+        return currentState
     }
 
     /**
@@ -134,16 +101,35 @@ class Store<out STATE, ACTION>(initialState: STATE,
      * It also immediately notifies the [subscriber] about the current State.
      *
      * @param subscriber subscriber to be notified on every State change
-     * @return the subscriber
+     * @return the subscriber, which can be passed to [unsubscribe] to cancel subscription
      */
-    fun subscribe(subscriber: StoreSubscriber<STATE>) {
-        if (isDispatching) {
-            throw IllegalStateException("You may not call store.subscribe() while the reducer is executing! If you would like to be notified after the store has been updated, subscribe from a component and invoke store.getState() in the callback to access the latest state.")
-        }
+    fun subscribe(subscriber: StoreSubscriber<STATE>): StoreSubscriber<STATE> {
+        if (isDispatching)
+            throw IllegalStateException("You may not call store.subscribe() while the Reducer is executing! If you would like to be notified after the store has been updated, subscribe from a component and invoke store.getState() in the callback to access the latest state.")
 
         subscribers += subscriber
         // Immediately notify new subscriber about the current State.
         subscriber.onNewState(currentState)
+
+        return subscriber
+    }
+
+    /**
+     * Subscribes [onNewState] callback to the State changes.
+     *
+     * This basically creates [StoreSubscriber] under-the-hood, and calls the given callback inside
+     * its [onNewState] method.
+     *
+     * @param onNewState callback to be called each time the State changes
+     * @return the subscriber, which can be passed to [unsubscribe] to cancel subscription
+     */
+    fun subscribe(onNewState: (STATE) -> Unit): StoreSubscriber<STATE> {
+        val subscriber = object : StoreSubscriber<STATE> {
+            override fun onNewState(state: STATE) = onNewState(state)
+            override fun onCommandReceived(command: Any) = Unit
+        }
+
+        return subscribe(subscriber)
     }
 
     /**

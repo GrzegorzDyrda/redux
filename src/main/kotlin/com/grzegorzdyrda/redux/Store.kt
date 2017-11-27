145c9b2be2db86f93b03c0f6c253eb2f467e1edf
Grzegorz Dyrda <grzegorz.dyrda@gmail.com>
2017-11-27 18:34:02 +0100

    Add missing comments

--- src/main/kotlin/com/grzegorzdyrda/redux/Store.kt
+++ src/main/kotlin/com/grzegorzdyrda/redux/Store.kt
@@ -5,6 +5,13 @@ import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.async
 import kotlin.coroutines.experimental.CoroutineContext
 
+/**
+ * Redux Store that holds the app's State.
+ *
+ * @constructor Creates the Redux Store
+ * @param initialState initial state of the Store
+ * @param reducer a function that returns the next State, given the current State and the Action
+ */
 class Store<out STATE, ACTION>(initialState: STATE,
                                private val reducer: (STATE, ACTION) -> STATE) {
 
@@ -23,6 +30,11 @@ class Store<out STATE, ACTION>(initialState: STATE,
         //listeners.forEach { it(currentState) }
     }
 
+    /**
+     * Dispatches an Action. It's the only way to trigger a State change.
+     *
+     * This method is thread-safe. Can be called from any thread.
+     */
     fun dispatch(action: ACTION): ACTION {
         if (isDispatching) {
             throw IllegalStateException("Reducers may not dispatch actions! They should be pure functions - no side effects at all.")
@@ -46,6 +58,9 @@ class Store<out STATE, ACTION>(initialState: STATE,
         return action
     }
 
+    /**
+     * Dispatches a *Command*. Commands are a way of notifying that some side-effect should be performed.
+     */
     fun dispatchCommand(command: Any): Any {
         subscribers.forEach { it.onCommandReceived(command) }
 
@@ -55,18 +70,20 @@ class Store<out STATE, ACTION>(initialState: STATE,
     /**
      * Convenient method to dispatch more complicated logic.
      *
-     * [block] function receives [dispatch] and [getState] functions as parameters
+     * @param block function to be called. It receives [dispatch] and [getState] functions as parameters
      */
     fun <R> dispatch(block: (dispatch: (ACTION) -> ACTION, getState: () -> STATE) -> R): R {
         return block(this::dispatch, this::getState)
     }
 
     /**
-     * Convenient method to dispatch more complicated logic that involves coroutines.
+     * Convenient method to dispatch asynchronous logic by utilizing coroutines.
      *
-     * You may call
+     * The [block] parameter is a *suspending lambda*, thus allows calling any suspending
+     * function inside.
      *
-     * @param context: CoroutineContext
+     * @param context coroutine context. If not specified [DefaultDispatcher] will be used
+     * @param block suspending lambda - the coroutine code
      */
     fun <R> dispatchAsync(context: CoroutineContext = DefaultDispatcher,
                           block: suspend (dispatch: (ACTION) -> ACTION, getState: () -> STATE) -> R): Deferred<R> {
@@ -75,16 +92,19 @@ class Store<out STATE, ACTION>(initialState: STATE,
         }
     }
 
+    /**
+     * Returns the current State kept by the Store.
+     */
     fun getState(): STATE {
         if (isDispatching) {
-            throw IllegalStateException("You may not call store.getState() while the reducer is executing! The reducer has already received the state as an argument. Pass it down from the top reducer instead of reading it from the store.")
+            throw IllegalStateException("You may not call store.getState() while the reducer is executing! The reducer has already received the State as an argument. Pass it down from the top Reducer instead of reading it from the store.")
         }
 
         return currentState
     }
 
     /**
-     * Subscribes [listener] to the State changes.
+     * Subscribes the [listener] to the State changes.
      *
      * Also, immediately fires the [listener], giving him the current State.
      *
@@ -108,6 +128,14 @@ class Store<out STATE, ACTION>(initialState: STATE,
         }
     }
 
+    /**
+     * Subscribes the [subscriber] to this store's State changes.
+     *
+     * It also immediately notifies the [subscriber] about the current State.
+     *
+     * @param subscriber subscriber to be notified on every State change
+     * @return the subscriber
+     */
     fun subscribe(subscriber: StoreSubscriber<STATE>) {
         if (isDispatching) {
             throw IllegalStateException("You may not call store.subscribe() while the reducer is executing! If you would like to be notified after the store has been updated, subscribe from a component and invoke store.getState() in the callback to access the latest state.")
@@ -118,6 +146,9 @@ class Store<out STATE, ACTION>(initialState: STATE,
         subscriber.onNewState(currentState)
     }
 
+    /**
+     * Unsubscribes the given [subscriber] from this store's State changes.
+     */
     fun unsubscribe(subscriber: StoreSubscriber<STATE>) {
         subscribers -= subscriber
     }
